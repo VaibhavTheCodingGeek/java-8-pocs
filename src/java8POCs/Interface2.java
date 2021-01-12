fd364d932b04118d1d09bb34b8bf5553b7c4e88b
VaibhavThaCoder <vaibhav92jun@gmail.com>
2021-01-12 14:37:17 +0530

    Annotation @FunctionalInterface benefits

--- src/java8POCs/Interface2.java
+++ src/java8POCs/Interface2.java
@@ -1,5 +1,8 @@
 package java8POCs;
 
+// explicitly specifying that the  interface is a FunctionalInterface and please dont add any other abstract methods in it
+
+@FunctionalInterface
 public interface Interface2 {
 
 	public int getLength(String s);
