d2499f807f33cff4cc39a47c4cd99f256e5344c0
VaibhavThaCoder <vaibhav92jun@gmail.com>
2021-01-29 18:25:46 +0530

    Primitive type functional interface - predicate.

---
+++ src/PrimitiveTypeFunctionalInterface/IntPredicateDemo.java
@@ -0,0 +1,21 @@
+package PrimitiveTypeFunctionalInterface;
+
+import java.util.function.IntPredicate;
+
+public class IntPredicateDemo {
+
+	public static void main(String[] args) {
+
+		// writing a Primitive type(int) predicate for perfomance improvement
+		// and avoiding Autoboxing and Autounboxing.
+		IntPredicate p = i -> i % 2 == 0;
+
+		int[] x = {0, 5, 10, 15, 20, 25};
+		for (int x1 : x) {
+			if (p.test(x1)) {
+				System.out.println(x1);
+			}
+		}
+	}
+
+}
