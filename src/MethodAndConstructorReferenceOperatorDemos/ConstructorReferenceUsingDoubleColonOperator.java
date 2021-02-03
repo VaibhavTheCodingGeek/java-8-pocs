80ff55eb0bebf31254dd55992a9dc24b29807352
VaibhavThaCoder <vaibhav92jun@gmail.com>
2021-02-03 18:44:23 +0530

    Simplication in POC for Constructor Reference by :: operator.

--- src/MethodAndConstructorReferenceOperatorDemos/ConstructorReferenceUsingDoubleColonOperator.java
+++ src/MethodAndConstructorReferenceOperatorDemos/ConstructorReferenceUsingDoubleColonOperator.java
@@ -3,21 +3,15 @@ package MethodAndConstructorReferenceOperatorDemos;
 public class ConstructorReferenceUsingDoubleColonOperator {
 
 	public static void main(String[] args) {
-		// here we want to invoke get() method from interface interf1 which
-		// returns Sample object
 
-		// the Sample() constructor referenced consisted print statement which
-		// is then displayed using get() method
 
 		Interf1 i = Sample::new;
 
-		// in short the above statement refers to Sample() constrcutor using ::
+		// the above statement refers to Sample() constrcutor using ::
 		// operator
 
 		// thus get() internally refers to Sample class no arg constructor
-
-		// to get the sample object we used get() method
-		Sample s = i.get();
+		i.get();
 
 	}
 
--- src/MethodAndConstructorReferenceOperatorDemos/Interf1.java
+++ src/MethodAndConstructorReferenceOperatorDemos/Interf1.java
@@ -1,6 +1,6 @@
 package MethodAndConstructorReferenceOperatorDemos;
 
 public interface Interf1 {
-	public Sample get();
+	public void get();
 
 }
